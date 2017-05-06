715a05a69fb36cde54da80a4f9fde1b93b8fb5e5
Rodrigo Valin <licorna@gmail.com>
2017-05-06 01:07:36 +0100

    Removed unused macro.

--- doom-cl.lisp
+++ doom-cl.lisp
@@ -28,14 +28,6 @@
   `(let ,(loop for n in names collect `(,n (make-symbol ,(string n))))
      ,@body))
 
-;; from the book
-(defmacro once-only ((&rest names) &body body)
-  (let ((gensyms (loop for n in names collect (gensym (string n)))))
-    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
-       `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
-          ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
-             ,@body)))))
-
 (defun as-keyword (sym) (intern (string sym) :keyword))
 
 (defun slot->defclass-slot (spec)
